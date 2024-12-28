0e4692b602b2ae6813e742a39d2eeca73203e870
Alejandro Otero <aleoterob@gmail.com>
2024-12-27 23:46:11 -0300

    Adds responsiveness

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,10 +9,12 @@ export const metadata: Metadata = {
 export default function Contact() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="pt-12 text-8xl text-center">
+      <h1 className="pt-12 text-4xl sm:text-6xl md:text-6xl lg:text-6xl  xl:text-8xl text-center">
         Next.js and TailWind Project
       </h1>
-      <h1 className="pt-4  pb-6 text-4xl text-center">Custom Form Component</h1>
+      <h1 className="pt-4  pb-6 text-2xl sm:text-4xl md:text-4xl lg:text-4xl text-center">
+        Custom Form Component
+      </h1>
       <ContactForm />
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
   return (
     <div className="">
       <main className="flex flex-col items-center">
-        <h1 className="pt-12 text-8xl text-center">
+        <h1 className="pt-12 text-4xl sm:text-6xl md:text-6xl lg:text-6xl  xl:text-8xl text-center">
           Next.js and TailWind Project
         </h1>
-        <h1 className="pt-4  pb-6 text-4xl text-center">
+        <h1 className="pt-4  pb-6 text-2xl sm:text-4xl md:text-4xl lg:text-4xl text-center">
           Stacked List Component that consumes Unsplash API
         </h1>
-        <div className="flex flex-row gap-10">
+        <div className="flex flex-row gap-10 flex-wrap justify-center">
           <StackedList />
           <StackedList />
           <StackedList />
